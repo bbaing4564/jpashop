e731af5ddf3ff1376385ab61d0ec21a0521a0b78
bbaing4564 <bbaing4564@gmail.com>
2021-02-03 06:46:18 +0900

    Order, OrderItem entity 로직 추가

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -1,16 +1,20 @@
 package jpabook.jpashop.domain;
 
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @Entity
 @Table(name = "orders")
 @Getter @Setter
+@NoArgsConstructor
 public class Order {
 
     @Id @GeneratedValue
@@ -32,6 +36,8 @@ public class Order {
     @Enumerated(EnumType.STRING)
     private OrderStatus status; //주문상태 [ORDER, CANCEL]
 
+
+
     //==연관관계 메서드==//
     public void setMember(Member member) {
         this.member = member;
@@ -44,6 +50,49 @@ public class Order {
     public void setDelivery(Delivery delivery) {
         this.delivery = delivery;
         delivery.setOrder(this);
+    }
+
+    //==생성 메서드==//
+//    public static Order createOrder(Member member, Delivery delivery, OrderItem... orderItems) {
+//        Order order = new Order();
+//        order.setMember(member);
+//        order.setDelivery(delivery);
+//        for (OrderItem orderItem : orderItems) {
+//            order.addOrderItem(orderItem);
+//        }
+//        order.setStatus(OrderStatus.ORDER);
+//        order.setOrderDate(LocalDateTime.now());
+//        return order;
+//    }
+
+    // builder
+    @Builder
+    private Order(Member member, Delivery delivery, OrderItem... orderItems) {
+        this.member = member;
+        this.delivery = delivery;
+        Arrays.stream(orderItems).forEach(this::addOrderItem);
+    }
+
+    //== 비즈니스 로직 ==//
+    /**
+     * 주문 취소
+     */
+    public void cancel() {
+        if (delivery.getStatus() == DeliveryStatus.COMP) {
+            throw new IllegalStateException("이미 배송완료 된 상품은 취소가 불가합니다.");
+        }
+        this.setStatus(OrderStatus.CANCEL);
+        orderItems.forEach(OrderItem::cancel);
+    }
+
+    //==조회 로직==//
+    /**
+     * 전체 주문 가격 조회
+     */
+    public int getTotalPrice() {
+        return orderItems.stream()
+                .mapToInt(OrderItem::getOrderPrice)
+                .sum();
     }
 
 }
--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -1,12 +1,16 @@
 package jpabook.jpashop.domain;
-import lombok.Getter;
-import lombok.Setter;
+
 import jpabook.jpashop.domain.item.Item;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.*;
 
 @Entity
 @Table(name = "order_item")
 @Getter @Setter
+@NoArgsConstructor
 public class OrderItem {
 
     @Id @GeneratedValue
@@ -25,4 +29,33 @@ public class OrderItem {
     private int orderPrice; //주문 가격
 
     private int count; //주문 수량
+
+    //== 생성 메서드 ==//
+//    public static OrderItem createOrderItem(Item item, int orderPrice, int count) {
+//        OrderItem orderItem = new OrderItem();
+//        orderItem.setItem(item);
+//        orderItem.setOrderPrice(orderPrice);
+//        orderItem.setCount(count);
+//
+//        item.removeStock(count);
+//        return orderItem;
+//    }
+
+    private OrderItem(Item item, int orderPrice, int count) {
+        this.item = item;
+        this.orderPrice = orderPrice;
+        this.count = count;
+
+        item.removeStock(count);
+    }
+
+    //== 비즈니스 로직 ==//
+    public void cancel() {
+        getItem().addStock(count);
+    }
+
+    //== 조회 로직 ==//
+    public int getTotalPrice() {
+        return getOrderPrice() * getCount();
+    }
 }
