f161a6869f56bde768339d7e82ff50bbb7dcd22b
bbaing4564 <bbaing4564@gmail.com>
2021-02-06 07:22:01 +0900

    Optimize import

--- src/main/java/jpabook/jpashop/domain/Category.java
+++ src/main/java/jpabook/jpashop/domain/Category.java
@@ -1,8 +1,8 @@
 package jpabook.jpashop.domain;
 
+import jpabook.jpashop.domain.item.Item;
 import lombok.Getter;
 import lombok.Setter;
-import jpabook.jpashop.domain.item.Item;
 
 import javax.persistence.*;
 import java.util.ArrayList;
--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -2,11 +2,9 @@ package jpabook.jpashop.repository;
 
 import jpabook.jpashop.domain.Order;
 import lombok.RequiredArgsConstructor;
-import org.aspectj.weaver.ast.Or;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
-import java.util.List;
 
 @Repository
 @RequiredArgsConstructor
@@ -22,6 +20,8 @@ public class OrderRepository {
         return em.find(Order.class, orderId);
     }
 
-//    public List<Order> findAll(OrderSearch orderSearch) { }
+//    public List<Order> findAll(OrderSearch orderSearch) {
+//        return null;
+//    }
 
 }
