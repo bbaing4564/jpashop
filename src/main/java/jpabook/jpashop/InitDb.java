ab3fddb89971ddaef3a66576ab93553d85530cad
bbaing4564 <bbaing4564@gmail.com>
2021-02-12 09:14:18 +0900

    add default entities setting

--- src/main/java/jpabook/jpashop/InitDb.java
+++ src/main/java/jpabook/jpashop/InitDb.java
@@ -1,14 +1,24 @@
 package jpabook.jpashop;
 
+import jpabook.jpashop.domain.*;
+import jpabook.jpashop.domain.item.Book;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
 import javax.persistence.EntityManager;
 import javax.transaction.Transactional;
 
 @Component
 @RequiredArgsConstructor
 public class InitDb {
+
+    private final InitService initService;
+
+    @PostConstruct
+    public void init() {
+        initService.dbInit1();
+    }
 
     @Component
     @Transactional
@@ -17,7 +27,35 @@ public class InitDb {
         private final EntityManager em;
 
         public void dbInit1() {
+            Member member1 = new Member();
+            member1.setName("member1");
+            member1.setAddress(new Address("Seoul", "street", "123"));
+            em.persist(member1);
 
+            Book book1 = new Book();
+            book1.setName("JPA1");
+            book1.setPrice(10000);
+            book1.setStockQuantity(100);
+            em.persist(book1);
+
+            Book book2 = new Book();
+            book2.setName("JPA2");
+            book2.setPrice(20000);
+            book2.setStockQuantity(100);
+            em.persist(book2);
+
+            OrderItem orderItem1 = OrderItem.createOrderItem(book1, 10000, 1);
+            OrderItem orderItem2 = OrderItem.createOrderItem(book2, 20000, 2);
+
+            Delivery delivery = new Delivery();
+            delivery.setAddress(member1.getAddress());
+            Order order = Order.createOrder(member1, delivery, orderItem1, orderItem2);
+            em.persist(order);
+
+            Member member2 = new Member();
+            member2.setName("member2");
+            member2.setAddress(new Address("Seoul", "street", "123"));
+            em.persist(member2);
         }
 
     }
